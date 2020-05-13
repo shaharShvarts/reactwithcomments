688c88098f517d02528870c568c7e7310dfd0506
shahar shvarts <shahar.shvarts@gmail.com>
2020-05-13 22:25:21 +0300

    Updates

---
+++ precache-manifest.4fb8f10c9e162c6b45fa131dbe476098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54697b81a0da4fbb56c20bdaa3c09239",
+    "url": "/reactwithcomments/index.html"
+  },
+  {
+    "revision": "c1ca120b83de57a7634b",
+    "url": "/reactwithcomments/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "292c45c5a0495a8f192f",
+    "url": "/reactwithcomments/static/js/2.081b78ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactwithcomments/static/js/2.081b78ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ca120b83de57a7634b",
+    "url": "/reactwithcomments/static/js/main.b8e64fe7.chunk.js"
+  },
+  {
+    "revision": "fe27a6e6e16ffa204bfe",
+    "url": "/reactwithcomments/static/js/runtime-main.995a0567.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactwithcomments/static/media/logo.5d5d9eef.svg"
+  }
+]);
